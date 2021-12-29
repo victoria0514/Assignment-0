af83e6031f45ffde650017bbe7a93a29ba8e257c
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2021-12-29 15:58:04 -0500

    second init

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,11 +1,11 @@
 function titleCaseEdit(title) 
 {
-  return title.replace(/\w\S*/g, function (txt) {
+  return title.replace(/\w\S*/g, function (txt) 
+  {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   }
   
-  )
-  ;
+  );
   
   
 }
